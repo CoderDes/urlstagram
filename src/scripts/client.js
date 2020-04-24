9f8c120340354c6bc1893e3409260e24d5d3f27c
Eugene <eugzubv@gmail.com>
2020-04-24 16:50:46 +0300

    add request from client to server

--- src/scripts/client.js
+++ src/scripts/client.js
@@ -1,1 +1,26 @@
-console.log("THIS IS CLIENT JAVASCRIPT!");
+class UrlParser {
+  url = "https://localhost:3000/url-parse";
+
+  constructor(selector) {
+    this.parser = document.querySelector(selector);
+    this.input = this.parser.querySelector(".input");
+    this.submit = this.parser.querySelector(".button--submit");
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(event) {
+    fetch(this.url, {
+      method: "POST",
+      body: JSON.stringify(this.getInputData()),
+    });
+  }
+  getInputData() {
+    return this.input.value;
+  }
+  init() {
+    this.submit.addEventListener("click", this.onSubmit);
+  }
+}
+
+const urlParser = new UrlParser(".url-parser");
+urlParser.init();
