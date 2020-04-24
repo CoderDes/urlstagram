14339dff80cf8c5bdcd5eb69e618aff46861ba58
Eugene <eugzubv@gmail.com>
2020-04-24 15:53:50 +0300

    basic setup

---
+++ router.js
@@ -0,0 +1,17 @@
+const { createReadStream } = require("fs");
+const { join } = require("path");
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.render("home.nj");
+});
+
+router.get(/.(js|css)$/, (req, res) => {
+  const { path } = req;
+  const rs = createReadStream(join(__dirname, path));
+  rs.pipe(res);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+const router = require("./router.js");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+nunjucks.configure("view", { autoescape: true, express: app });
+
+app.use(router);
+
+app.listen(port, () => console.log(`SERVER STARTED ON PORT ${port}`));
